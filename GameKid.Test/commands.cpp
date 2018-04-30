3393e7e7436658f5a804b5caec52b5317fb431f2
repnz <repnzb@gmail.com>
2018-05-01 01:21:05 +0300

    Added RL_3 test, rename RL_n to RL_1

--- GameKid.Test/commands.cpp
+++ GameKid.Test/commands.cpp
@@ -2,10 +2,18 @@
 #include <GameKid/cpu.h>
 
 
-TEST(Commands, RL_n) {
+TEST(Commands, RL_1) {
 	cpu c;
 	byte val = 1<<7; 
 	c.rl_n(&val, 1);
 	ASSERT_EQ(val, 1) << "bit 7 did not move to place 0";
+	ASSERT_EQ(c.carry_flag, 1) << "carry flag did not update correctly";
+}
+
+TEST(Commands, RL_3) {
+	cpu c;
+	byte val = 0b10101000;
+	c.rl_n(&val, 3);
+	ASSERT_EQ(val, 0b01000101);
 	ASSERT_EQ(c.carry_flag, 1) << "carry flag did not update correctly";
 }
