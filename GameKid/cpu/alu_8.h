3e42dd0c08983a456a0989b137d26444582e3d65
repnz <repnzb@gmail.com>
2018-05-08 01:02:15 +0300

    Added sub_instruction

--- GameKid/cpu/alu_8.h
+++ GameKid/cpu/alu_8.h
@@ -1,2 +1,24 @@
 #pragma once
 #include <GameKid/cpu/instruction.h>
+#include "GameKid/opcodes.h"
+
+class sub_instruction : register_instruction
+{
+public:
+	explicit sub_instruction(cpu& cpu)
+		: register_instruction(cpu, "sub",
+			SUB_A_A, 
+			SUB_A_B, 
+			SUB_A_C,
+			SUB_A_D,
+			SUB_A_E, 
+			SUB_A_H,
+			SUB_A_L)
+	{
+	}
+
+	void run_on_byte(byte* val) override
+	{
+		_cpu.sub(&_cpu.A, *val);
+	}
+};
