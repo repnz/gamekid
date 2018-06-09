ac163044b6b2cf0f987013e37af7410bc069d1c3
repnz <repnzb@gmail.com>
2018-06-09 10:49:47 +0300

    Add JP cc, nn opcodes

--- GameKid/cpu/instructions/jumps.cpp
+++ GameKid/cpu/instructions/jumps.cpp
@@ -8,6 +8,14 @@ void jump_absolute_operation(cpu& cpu, operand<word>& address)
     cpu.jump(address.load());
 }
 
+void jump_absolute_with_condition_operation(cpu& cpu, operand<bool>& condition, operand<word>& address)
+{
+    if (condition.load())
+    {
+        cpu.jump(address.load());
+    }
+}
+
 void jumps::initialize(cpu & cpu, instruction_set & set)
 {
     set.add_instruction(instruction_builder(cpu, "jp")
@@ -15,6 +23,26 @@ void jumps::initialize(cpu & cpu, instruction_set & set)
             .opcode(0xC3)
             .cycles(12)
             .operation(jump_absolute_operation)
+            .add()
+        .operands(cpu.operands().nz(), cpu.operands().immidiate_word())
+            .opcode(0xC2)
+            .cycles(12)
+            .operation(jump_absolute_with_condition_operation)
+            .add()
+        .operands(cpu.operands().z(), cpu.operands().immidiate_word())
+            .opcode(0xCA)
+            .cycles(12)
+            .operation(jump_absolute_with_condition_operation)
+            .add()
+        .operands(cpu.operands().nc(), cpu.operands().immidiate_word())
+            .opcode(0xD2)
+            .cycles(12)
+            .operation(jump_absolute_with_condition_operation)
+            .add()
+        .operands(cpu.operands().c(), cpu.operands().immidiate_word())
+            .opcode(0xDA)
+            .cycles(12)
+            .operation(jump_absolute_with_condition_operation)
             .add()
         .build());
 }
