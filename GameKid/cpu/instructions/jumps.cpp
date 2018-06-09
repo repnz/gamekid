6ce203da6cf5f1ac7bae264d79eaa379e48c5b61
repnz <repnzb@gmail.com>
2018-06-09 11:04:56 +0300

    Rename jump_absolute to jp in operations

--- GameKid/cpu/instructions/jumps.cpp
+++ GameKid/cpu/instructions/jumps.cpp
@@ -3,12 +3,12 @@
 #include <GameKid/cpu/instruction_set.h>
 #include <GameKid/cpu/operands.h>
 
-void jump_absolute_operation(cpu& cpu, operand<word>& address)
+void jp_operation(cpu& cpu, operand<word>& address)
 {
     cpu.jump(address.load());
 }
 
-void jump_absolute_with_condition_operation(cpu& cpu, operand<bool>& condition, operand<word>& address)
+void jp_with_condition_operation(cpu& cpu, operand<bool>& condition, operand<word>& address)
 {
     if (condition.load())
     {
@@ -22,32 +22,37 @@ void jumps::initialize(cpu & cpu, instruction_set & set)
         .operands(cpu.operands().immidiate_word())
             .opcode(0xC3)
             .cycles(12)
-            .operation(jump_absolute_operation)
+            .operation(jp_operation)
             .add()
         .operands(cpu.operands().nz(), cpu.operands().immidiate_word())
             .opcode(0xC2)
             .cycles(12)
-            .operation(jump_absolute_with_condition_operation)
+            .operation(jp_with_condition_operation)
             .add()
         .operands(cpu.operands().z(), cpu.operands().immidiate_word())
             .opcode(0xCA)
             .cycles(12)
-            .operation(jump_absolute_with_condition_operation)
+            .operation(jp_with_condition_operation)
             .add()
         .operands(cpu.operands().nc(), cpu.operands().immidiate_word())
             .opcode(0xD2)
             .cycles(12)
-            .operation(jump_absolute_with_condition_operation)
+            .operation(jp_with_condition_operation)
             .add()
         .operands(cpu.operands().c(), cpu.operands().immidiate_word())
             .opcode(0xDA)
             .cycles(12)
-            .operation(jump_absolute_with_condition_operation)
+            .operation(jp_with_condition_operation)
             .add()
         .operands(cpu.operands().hl_addressing())
             .opcode(0xE9)
             .cycles(4)
-            .operation(jump_absolute_operation)
+            .operation(jp_operation)
+            .add()
+        .operands(cpu.operands().hl_addressing())
+            .opcode(0xE9)
+            .cycles(4)
+            .operation(jp_operation)
             .add()
         .build());
 }
