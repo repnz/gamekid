a56301febe563d50b16a78e8451fde02befc1dcc
repnz <repnzb@gmail.com>
2018-06-19 22:05:57 +0300

    Decompiler: create read_file function

--- GameKid.Decompiler/main.cpp
+++ GameKid.Decompiler/main.cpp
@@ -2,30 +2,24 @@
 #include <iostream>
 #include <fstream>
 
+std::vector<byte> read_file(const std::string& fileName);
+
 int main(int argc, const char** argv)
 {    
-    std::cout << "GameKidDecompiler" << std::endl;
-
-    cpu cpu;
-    instruction_set s(cpu);
-    opcode_decoder d(s);
-
     if (argc < 2)
     {
         std::cerr << "Missing filename argument";
         return -1;
     }
-
-    std::string fileName(argv[1]);
-    std::ifstream f(fileName, std::ios::binary | std::ios::in | std::ios::ate);
-    int file_size = f.tellg();
-    f.seekg(std::ios::beg);
-    std::vector<byte> opcodes(file_size);
-    f.read((char*)opcodes.data(), file_size);
     
+    std::vector<byte> opcodes = read_file(argv[1]);
     const byte* opcode_ptr = opcodes.data();
+
+    cpu cpu;
+    instruction_set s(cpu);
+    opcode_decoder d(s);
     
-    for (int i=0; i<file_size;)
+    for (int i=0; i<opcodes.size();)
     {
         const opcode* op = d.decode(opcode_ptr+i);
 
@@ -44,3 +38,14 @@ int main(int argc, const char** argv)
 
     return 0;
 }
+
+std::vector<byte> read_file(const std::string& fileName)
+{
+    std::ifstream f(fileName, std::ios::binary | std::ios::in | std::ios::ate);
+    int file_size = f.tellg();
+    f.seekg(std::ios::beg);
+    std::vector<byte> opcodes(file_size);
+    f.read((char*)opcodes.data(), file_size);
+    return opcodes;
+}
+
