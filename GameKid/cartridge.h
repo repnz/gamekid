f84ae38e793da9ab9811992416a11a2ba4cee128
repnz <repnzb@gmail.com>
2018-06-29 22:42:11 +0300

    Add cartridge_types enum

--- GameKid/cartridge.h
+++ GameKid/cartridge.h
@@ -27,6 +27,37 @@ namespace cartridge_offsets {
     const offset global_checksum(0x14e, 0x14f);
 };
 
+enum class cartrage_types {
+    rom_only = 0,
+    mbc1 = 0x01,
+    mbc1_ram = 0x02,
+    mbc1_ram_battery = 0x03,
+    mbc2 = 0x5,
+    mbc2_battery = 0x5,
+    rom_ram = 0x08,
+    rom_ram_battery = 0x09,
+    mmm01 = 0xb,
+    mmm01_ram = 0xc,
+    mmm01_ram_battery = 0xd,
+    mbc3_timer_ram_battery = 0xf,
+    mbc3_timer_ram_battery = 0x10,
+    mbc3 = 0x11,
+    mbc_ram = 0x12,
+    mbc3_ram_battery = 0x13,
+    mbc5 = 0x19,
+    mbc5_ram = 0x1a,
+    mbc5_ram_battery = 0x1b,
+    mbc5_rumble = 0x1c,
+    mbc5_rumble_ram = 0x1d,
+    mbc5_rumble_ram_battery = 0x1e,
+    mbc6 = 0x20,
+    mbc7_sensor_rumble_ram_battery = 0x22,
+    pocket_camera = 0xfc,
+    bandai_tama5 = 0xfd,
+    huc3 = 0xfe,
+    huc1_ram_battery = 0xff
+};
+
 namespace cartridge {
     byte calculate_header_checksum(const byte* data);
     bool validate_header_checksum(const byte* data);
