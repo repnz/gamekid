c9a578589591cca263dafcf8c59aaf192e2e332e
repnz <repnzb@gmail.com>
2018-07-14 13:40:47 +0300

    Debugger: Organize commands into functions and map

--- gamekid.debugger/main.cpp
+++ gamekid.debugger/main.cpp
@@ -3,52 +3,55 @@
 #include <gamekid/utils/files.h>
 #include <iostream>
 #include "gamekid/utils/str.h"
+#include "gamekid/cpu/operands_container.h"
 
-std::vector<std::string> get_command();
 
-int main(const int argc, const char** argv) 
+std::vector<std::string> get_user_command();
+
+using command = std::function<void(gamekid::runner& runner, const std::vector<std::string>& arguments)>;
+
+// Commands 
+void run(gamekid::runner& runner, const std::vector<std::string>& args);
+void add_breakpoint(gamekid::runner& runner, const std::vector<std::string>& args);
+void regs(gamekid::runner& runner, const std::vector<std::string>& args);
+void next(gamekid::runner& runner, const std::vector<std::string>& args);
+void list(gamekid::runner& runner, const std::vector<std::string>& args);
+void debugger_exit(gamekid::runner& runner, const std::vector<std::string>& args);
+
+bool debugger_running;
+
+int main(const int argc, const char** argv)
 {
     std::string filename(argv[1]);
     gamekid::rom::cartridge cart(gamekid::utils::files::read_file(filename));
     gamekid::runner r(std::move(cart));
-    
-    while (true){
-        std::vector<std::string> command = get_command();
+    std::map<std::string, command> commands =
+    {
+        { "run", run},
+        { "break", add_breakpoint},
+        { "regs", regs},
+        { "next", next},
+        { "list", list},
+        { "exit", debugger_exit },
+    };
 
-        if (command[0]== "run"){
-            r.run_until_break();
-        }
-        else if (command[0] == "break"){
-            const word address = std::stoi(command[1]);
-            r.add_breakpoint(address);
-        }
-        else if (command[0] == "regs") {
-            
-            for (gamekid::cpu::reg* reg : r.cpu().regs) {
-                std::cout << reg->name() << ": " << std::hex << reg->load_as_word() << std::endl;
-            }
+    debugger_running = true;
 
-        }
-        else if (command[0] == "next"){
-            r.next();
-            std::cout << r.list(1)[0] << std::endl;
-        }
-        else if (command[0] == "list"){
-            int count = std::stoi(command[1]);
-            std::vector<std::string> list = r.list(count);
+    while (debugger_running) {
+        std::vector<std::string> command = get_user_command();
 
-            for (auto& op : list) {
-                std::cout << op << std::endl;
-            }
-        } else {
-            std::cout << "Unknown command '" << gamekid::utils::str::join(command) << "'" << std::endl;
-        }
+        auto key_value_pair = commands.find(command[0]);
 
-        std::cout << std::endl;
+        if (key_value_pair == commands.end()) {
+            std::cout << "Unknown command '" << command[0] << "'" << std::endl;
+        }
+        else {
+            key_value_pair->second(r, command);
+        }
     }
 }
 
-std::vector<std::string> get_command() {
+std::vector<std::string> get_user_command() {
     std::cout << ">> ";
     std::vector<std::string> command;
 
@@ -59,4 +62,50 @@ std::vector<std::string> get_command() {
     } while (std::cin.peek() != '\n');
 
     return command;
+}
+
+void run(gamekid::runner& runner, const std::vector<std::string>& args) {
+    runner.run_until_break();
+}
+
+void add_breakpoint(gamekid::runner& runner, const std::vector<std::string>& args) {
+    if (args.size() <= 1) {
+        std::cerr << "breakpoint requires an address" << std::endl;
+        return;
+    }
+
+    const word address = gamekid::utils::convert::to_number<word>(args[1], 16);
+    runner.add_breakpoint(address);
+}
+
+void regs(gamekid::runner& runner, const std::vector<std::string>& args) {
+    for (gamekid::cpu::reg* reg : runner.cpu().regs) {
+        std::cout << reg->name() << ": " << std::hex << reg->load_as_word() << std::endl;
+    }
+}
+
+void next(gamekid::runner& runner, const std::vector<std::string>& args) {
+    runner.next();
+    std::cout << runner.list(1)[0] << std::endl;
+}
+
+void list(gamekid::runner& runner, const std::vector<std::string>& args) {
+    word count;
+
+    if (args.size() == 2) {
+        count = gamekid::utils::convert::to_number<word>(args[1]);
+    }
+    else {
+        count = 5;
+    }
+
+    std::vector<std::string> list = runner.list(count);
+
+    for (auto& op : list) {
+        std::cout << op << std::endl;
+    }
+}
+
+void debugger_exit(gamekid::runner& runner, const std::vector<std::string>& args) {
+    debugger_running = false;
 }
