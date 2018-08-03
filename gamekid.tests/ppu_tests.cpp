f68f7a4382a280cb5f8a0b00a42f036012f0c29f
repnz <repnzb@gmail.com>
2018-08-03 20:01:56 +0300

    tests: Add tile.get_color test

--- gamekid.tests/ppu_tests.cpp
+++ gamekid.tests/ppu_tests.cpp
@@ -3,6 +3,14 @@
 
 namespace gamekid::tests {
     
+    TEST(PPU, TILE_GET_COLOR) {
+        std::array<byte, 16> arr = { 0x7A, 0xA7, 0x8B, 0x9C};
 
+        video::io::tile t(arr);
+
+        ASSERT_EQ(0b10, t.get_color(0, 0));
+        ASSERT_EQ(0b00, t.get_color(1, 1));
+        ASSERT_EQ(0b11, t.get_color(0, 1));
+    }
    
 }
